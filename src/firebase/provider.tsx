45472c9c4c510a06bdc3830b2978a441c790b1bb
Marvin Rick Forcado <coolviper01@gmail.com>
2025-11-20 06:39:01 +0000

    hello can you remove the anonymous user account

--- src/firebase/provider.tsx
+++ src/firebase/provider.tsx
@@ -6,8 +6,6 @@ import { FirebaseApp } from 'firebase/app';
 import { Firestore } from 'firebase/firestore';
 import { Auth, User, onAuthStateChanged, signOut } from 'firebase/auth';
 import { FirebaseErrorListener } from '@/components/FirebaseErrorListener'
-import { initiateAnonymousSignIn } from './non-blocking-login';
-
 
 interface FirebaseProviderProps {
   children: ReactNode;
@@ -84,9 +82,6 @@ export const FirebaseProvider: React.FC<FirebaseProviderProps> = ({
     const unsubscribe = onAuthStateChanged(
       auth,
       (firebaseUser) => { // Auth state determined
-        if (!firebaseUser) {
-          initiateAnonymousSignIn(auth);
-        }
         setUserAuthState({ user: firebaseUser, isUserLoading: false, userError: null });
       },
       (error) => { // Auth listener error
